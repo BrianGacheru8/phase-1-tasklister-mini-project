82481a099663daf9d822928cb10af1b09cc5f41c
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-06 18:12:53 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  btn=document.getElementById("new-task-description")
-})
+btn=document.getElementById("new-task-description")
 btn.addEventListener("click", (event)=>{
-   VL=document.getElementById("new-task-description").value
-   LI=document.createElement("li");
-   LI.innerText=`${VL}`
-   TSK=document.getElementById("tasks");
-   TSK.appendChild(LI)
-   event.preventDefault() 
+    VL=document.getElementById("new-task-description").value
+    LI=document.createElement("li");
+    LI.innerText=`${VL}`
+    TSK=document.getElementById("tasks");
+    TSK.appendChild(LI)
+    event.preventDefault() 
   })
-
